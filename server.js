7723c5ab7d1ca7a4a6fd524ac456966e697cfb97
mehul2015 <mehuldev2015@gmail.com>
2022-03-10 04:35:40 +0530

    Benchmark

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 //clustering
+process.env.UV_THREADPOOL_SIZE = 1;
 
 const cluster = require('cluster');
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 if (cluster.isMaster) {
   cluster.fork();
   cluster.fork();
-  cluster.fork();
-  //multiple copies of our server running
 } else {
   app.get('/', (req, res, next) => {
-    res.send('Hello, World!');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hello, World!');
+    });
   });
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} `);
